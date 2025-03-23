94f219e06f227e45aba501d957948208e43319e7
Yin <yinnndlovu@gmail.com>
2025-03-23 12:57:03 +0100

    Format static pages

--- src/data/content.ts
+++ src/data/content.ts
@@ -3,12 +3,13 @@ const content = {
 aboutMeText: `Hi! I am Yin. I am currently a Computer Science final-year student. I am soooooo close to
 graduating though, my school just needs me to complete an internship that runs
 for at least six month in order for me to be able to fulfill my graduation
-requirements.
+requirements. <br></br>
+
             
 I am currently doing that internship. It takes up much of my time currently,
 So I am not focused much on personal projects currently, although I do tend
 to them from time to time. I'm just super excited to be doing this internship
-as a software developer.
+as a software developer. <br></br>
             
 This is my personal website (also still in the works) and below you can find the things I
 know so far, my expertise and things I am currently focused on.`,
@@ -28,7 +29,7 @@ It also has a
 with it that carries 33,000+ members currently.
 So seeing that as it is, inspired my new idea for a project and
 I am going to create a mobile app for it, all about Liverpool.
-
+<br></br>
 It's an exciting project for me and due to familiarity with the
 Android ecosystem, Java, Kotling and stuff,
 I have decided to just start with an
@@ -36,7 +37,7 @@ Android application before anything else. After I'm successful in creation
 of that then I'm going to focus on producing an iOS application for it too
 which is probably going to be waaaaay later. (That's if I don't get drawn
 into another project after I finish this, but we'll see).
-
+<br></br>
 The idea behind this application is to give my followers and Liverpool
 fans in general an independent platform of all things Liverpool.
 This app is going to have everything, and everything about Liverpool,
