55a72f9259877ae7419e84a73bcc430472140730
Yin <162073364+ynhx@users.noreply.github.com>
2025-05-28 08:36:38 +0200

    Add C# and ASP.NET Core icon to my tech stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,4 +1,4 @@
-import './techstack.css';
+import "./techstack.css";
 
 import javaLogo from "../assets/java_logo.png";
 import kotlinLogo from "../assets/kotlin_logo.png";
@@ -12,48 +12,63 @@ import sqlLogo from "../assets/sql_logo.webp";
 import mysqlLogo from "../assets/mysql_logo.png";
 import gitLogo from "../assets/git_logo.png";
 import reactLogo from "../assets/react_logo.png";
+import csharpLogo from "../assets/csharp_logo.png";
+import netCoreLogo from "../assets/netcore_logo.svg.png";
 
 const techStack = [
-    { name: "Java", logo: javaLogo },
-    { name: "Kotlin", logo: kotlinLogo },
-    { name: "JavaScript", logo: jsLogo },
-    { name: "TypeScript", logo: tsLogo },
-    { name: "Node.js", logo: nodeLogo },
-    { name: "HTML", logo: htmlLogo },
-    { name: "CSS", logo: cssLogo },
-    { name: "Spring Boot", logo: springLogo },
-    { name: "SQL", logo: sqlLogo },
-    { name: "MySQL", logo: mysqlLogo },
-    { name: "Git", logo: gitLogo },
-    { name: "React", logo: reactLogo }
+  { name: "Java", logo: javaLogo },
+  { name: "Kotlin", logo: kotlinLogo },
+  { name: "JavaScript", logo: jsLogo },
+  { name: "TypeScript", logo: tsLogo },
+  { name: "Node.js", logo: nodeLogo },
+  { name: "HTML", logo: htmlLogo },
+  { name: "CSS", logo: cssLogo },
+  { name: "Spring Boot", logo: springLogo },
+  { name: "SQL", logo: sqlLogo },
+  { name: "MySQL", logo: mysqlLogo },
+  { name: "Git", logo: gitLogo },
+  { name: "React", logo: reactLogo },
+  { name: "C#", logo: csharpLogo },
+  { name: ".NET Core", logo: netCoreLogo },
 ];
 
 const TechStack = () => {
+  const duplicatedTechStack = [
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+    ...techStack,
+  ];
 
-    const duplicatedTechStack = [
-        ...techStack, ...techStack, ...techStack, ...techStack,
-        ...techStack, ...techStack, ...techStack, ...techStack,
-        ...techStack, ...techStack, ...techStack, ...techStack,
-        ...techStack, ...techStack, ...techStack, ...techStack
-    ];
-
-    return (
-        <div>
-            <h2 className="tech-stack-title">MY TECH STACK</h2>
-            <div className="tech-stack">
-                <div className="tech-stack-slider">
-                    {duplicatedTechStack.map((tech, index) => (
-                        <img
-                            key={index}
-                            src={tech.logo}
-                            alt={tech.name}
-                            className="tech-logo"
-                        />
-                    ))}
-                </div>
-            </div>
+  return (
+    <div>
+      <h2 className="tech-stack-title">MY TECH STACK</h2>
+      <div className="tech-stack">
+        <div className="tech-stack-slider">
+          {duplicatedTechStack.map((tech, index) => (
+            <img
+              key={index}
+              src={tech.logo}
+              alt={tech.name}
+              className="tech-logo"
+            />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TechStack;
